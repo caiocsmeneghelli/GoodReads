510888fd32848b1c767118a36f9982ffa2322007
Caio Cesar <caiocsmeneghelli@outlook.com>
2024-09-07 17:36:38 -0300

    Add: BookService com busca por ISBN.

---
+++ GoodReads.Core/Services/IBookService.cs
@@ -0,0 +1,14 @@
+﻿using GoodReads.Core.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodReads.Core.Services
+{
+    public interface IBookService
+    {
+        Task<BookDto?> SearchBookByISBN(string ISBN);
+    }
+}
---
+++ GoodReads.Infrastructure/Services/BookService.cs
@@ -0,0 +1,32 @@
+﻿using GoodReads.Core.DTOs;
+using GoodReads.Core.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodReads.Infrastructure.Services
+{
+    internal class BookService : IBookService
+    {
+        private readonly string _baseUri = "http://viacep.com.br/ws/{0}/json";
+        public async Task<BookDto?> SearchBookByISBN(string ISBN)
+        {
+            HttpClient httpClient = new HttpClient();
+            var url = string.Format(_baseUri, ISBN);
+            httpClient.BaseAddress = new Uri(url);
+
+            HttpResponseMessage response = httpClient.GetAsync("").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                BookDto parseObject = response.Content.ReadFromJsonAsync<BookDto>().Result;
+                return parseObject;
+            }
+
+            return null;
+        }
+    }
+}
